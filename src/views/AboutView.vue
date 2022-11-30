f94343084253ad5f928de161bea522173733949b
Michael <michaelnz10@outlook.com>
2022-11-30 23:02:32 +1300

    Initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <h1>The Cat Says Meow</h1>
+    <p>Dantooine. They're on Dantooine. I want to come with you to Alderaan. There's nothing for me here now. I want to learn the ways of the Force and be a Jedi, like my father before me. I find your lack of faith disturbing.
+
+      In my experience, there is no such thing as luck. You mean it controls your actions? I don't know what you're talking about. I am a member of the Imperial Senate on a diplomatic mission to Alderaan-- Don't act so surprised, Your Highness. You weren't on any mercy mission this time. Several transmissions were beamed to this ship by Rebel spies. I want to know what happened to the plans they sent you.
+
+      Kid, I've flown from one side of this galaxy to the other. I've seen a lot of strange stuff, but I've never seen anything to make me believe there's one all-powerful Force controlling everything. There's no mystical energy field that controls my destiny. It's all a lot of simple tricks and nonsense. You're all clear, kid. Let's blow this thing and go home!
+
+      I suggest you try it again, Luke. This time, let go your conscious self and act on instinct. What?! The Force is strong with this one. I have you now. I need your help, Luke. She needs your help. I'm getting too old for this sort of thing.
+
+      You're all clear, kid. Let's blow this thing and go home! The plans you refer to will soon be back in our hands. The plans you refer to will soon be back in our hands. He is here. Don't underestimate the Force.
+
+      He is here. Alderaan? I'm not going to Alderaan. I've got to go home. It's late, I'm in for it as it is. You're all clear, kid. Let's blow this thing and go home! Partially, but it also obeys your commands.
+
+      No! Alderaan is peaceful. We have no weapons. You can't possibly… The plans you refer to will soon be back in our hands. In my experience, there is no such thing as luck. I want to come with you to Alderaan. There's nothing for me here now. I want to learn the ways of the Force and be a Jedi, like my father before me.</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1>Big Purrs from Emmy</h1>
+    <p>A true inspiration for the children. It's just like the story of the grasshopper and the octopus. All year long, the grasshopper kept burying acorns for winter, while the octopus mooched off his girlfriend and watched TV. But then the winter came, and the grasshopper died, and the octopus ate all his acorns. Also he got a race car. Is any of this getting through to you?
+
+      Noooooo! We don't have a brig. Actually, that's still true. Yeah, lots of people did. I usually try to keep my sadness pent up inside where it can fester quietly as a mental illness.
+
+      Who's brave enough to fly into something we all keep calling a death sphere? And when we woke up, we had these bodies. I'll tell them you went down prying the wedding ring off his cold, dead finger. You won't have time for sleeping, soldier, not with all the bed making you'll be doing.
+
+      Now that the, uh, garbage ball is in space, Doctor, perhaps you can help me with my sexual inhibitions? Whoa a real live robot; or is that some kind of cheesy New Year's costume? I'm sorry, guys. I never meant to hurt you. Just to destroy everything you ever believed in.
+
+      I was all of history's great robot actors - Acting Unit 0.8; Thespomat; David Duchovny! And from now on you're all named Bender Jr. Oh, but you can. But you may have to metaphorically make a deal with the devil. And by "devil", I mean Robot Devil. And by "metaphorically", I mean get your coat.</p>
+  </main>
+</template>
